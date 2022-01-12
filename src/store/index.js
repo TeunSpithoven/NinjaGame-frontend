31c4656c9d4f3140bbd205dd450aca29de6481d9
Teun Spithoven <teun.spithoven@gmail.com>
2022-01-12 12:56:49 +0100

    commenented store

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,17 @@
-import store from 'vuex';
-import Vuex from 'vuex';
+// import store from 'vuex';
+// import Vuex from 'vuex';
 
-export default store = new Vuex.Store({
-  state: {
-    roomName: '',
-    userName: '',
-  },
-  mutations: {
-    setRoomName (state, newRoomName) {
-      state.roomName = newRoomName;
-    },
-    setUserName (state, newUserName) {
-      state.userName = newUserName;
-    },
-  }
-})
+// export default store = new Vuex.Store({
+//   state: {
+//     roomName: '',
+//     userName: '',
+//   },
+//   mutations: {
+//     setRoomName (state, newRoomName) {
+//       state.roomName = newRoomName;
+//     },
+//     setUserName (state, newUserName) {
+//       state.userName = newUserName;
+//     },
+//   }
+// })
